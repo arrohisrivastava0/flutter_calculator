e60d4a3cc141b6dc2158fbc9771ea99f79851d52
arrohisrivastava <arrohisrivastava6@gmail.com>
2024-11-25 22:47:42 +0530

    Basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var userEqn='';
+  var userRes='';
   final List<String> buttons=[
     'AC','DEL','%','/',
-    '7','8','9','X',
+    '7','8','9','×',
     '4','5','6','-',
     '1','2','3','+',
     '00','0','.','='
@@ -37,7 +39,23 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(),
       body: Column(
         children: <Widget>[
-          Expanded(child: Container()),
+          Expanded(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                          alignment: Alignment.centerRight,
+                          child: Text(userEqn, style: TextStyle(fontSize: 40),)),
+                      Container(
+                          alignment: Alignment.bottomRight,
+                          child: Text(userRes, style: TextStyle(fontSize: 20),))
+                    ],
+                  ),
+                ),
+              )),
           Expanded(
               flex: 2,
               child: Container(
@@ -45,11 +63,61 @@ class _HomePageState extends State<HomePage> {
                   itemCount: buttons.length,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
                     itemBuilder: (BuildContext context, int index){
-                    return MyButton(
-                    buttonText: buttons[index],
-                    color: isSpecial(buttons[index])?Colors.deepPurple[300]:Colors.deepPurple[200],
-                    textColor: Colors.black,
-                    );
+                    if(buttons[index]=='='){
+                      return MyButton(
+                        onButtonTap: (){
+                          setState(() {
+                            userEqn ='';
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.deepPurple[50],
+                        textColor: Colors.black,
+                      );
+                    }
+                    else if(buttons[index]=='AC'){
+                      return MyButton(
+                        onButtonTap: (){
+                          setState(() {
+                            userEqn='';
+                            userRes='';
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.deepPurple[300],
+                        textColor: Colors.black,
+                      );
+                    }
+                    else if(buttons[index]=='DEL'){
+                      return MyButton(
+                        onButtonTap: (){
+                          setState(() {
+                            try{
+                              userEqn = userEqn.substring(0, userEqn.length - 1);
+                            }
+                            catch(e){
+                              userEqn='';
+                            }
+
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.deepPurple[300],
+                        textColor: Colors.black,
+                      );
+                    }
+                    else{
+                      return MyButton(
+                        onButtonTap: (){
+                          setState((){
+                            userEqn+=buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: isSpecial(buttons[index])?Colors.deepPurple[300]:Colors.deepPurple[200],
+                        textColor: Colors.black,
+                      );
+                    }
                     }),
               ))
         ],
@@ -59,7 +127,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   bool isSpecial(String s){
-    if(s=='AC'||s=='%'||s=='DEL'||s=='/'||s=='X'||s=='-'||s=='+'||s=='=')
+    if(s=='AC'||s=='%'||s=='DEL'||s=='/'||s=='×'||s=='-'||s=='+'||s=='=')
       return true;
     return false;
   }
--- lib/things/butons.dart
+++ lib/things/butons.dart
@@ -4,18 +4,25 @@ class MyButton extends StatelessWidget {
   final color;
   final textColor;
   final String buttonText;
+  final onButtonTap;
 
-  MyButton({this.color, this.textColor, required this.buttonText});
+
+  MyButton({this.color, this.textColor, required this.buttonText, this.onButtonTap});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius : BorderRadius.circular(20),
-        child: Container(
-          color: color,
-          child: Center(child: Text(buttonText, style: TextStyle(color: textColor),),),
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius : BorderRadius.circular(20),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(buttonText,
+                style: TextStyle(color: textColor, fontSize: 20),),),
+          ),
         ),
       ),
     );
